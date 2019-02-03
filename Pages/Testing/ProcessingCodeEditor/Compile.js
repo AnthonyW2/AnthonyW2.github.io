0a2fb868c9cb6b4b97d3f28bf38ae0ede53255fd
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-02-03 14:56:08 +1100

    Update Compile.js

--- Pages/Testing/ProcessingCodeEditor/Compile.js
+++ Pages/Testing/ProcessingCodeEditor/Compile.js
@@ -1,18 +1,24 @@
-function compile() {
-  var html = ""
+function compile(){
   var pjs = document.getElementById("pjs");
   var code = document.getElementById("code").contentWindow.document;
-
   document.body.onkeyup = function() {
     code.open();
     code.writeln(
-      html +
-      "<script type=\"text/processing\">" +
-      pjs.value +
-      "</script>"
+      "<!DOCTYPE html>"+
+      "<html>"+
+      "<head>"+
+      "<meta charset=\"utf-8\">"+
+      "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/processing.js/1.4.8/processing.min.js\"></script>"+
+      "</head>"+
+      "<body>"+
+      "<script type=\"text/processing\" data-processing-target=\"processing-canvas\">"+
+      pjs.value+
+      "</script>"+
+      "<canvas id=\"processing-canvas\"> </canvas>"+
+      "</body>"+
+      "</html>"
     );
     code.close();
   };
 }
-
 compile();
