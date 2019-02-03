2890f41e7d3217f40a7f262ac0b14fed625b136f
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-02-03 12:59:42 +1100

    Update Compile.js

--- Pages/Testing/ProcessingCodeEditor/Compile.js
+++ Pages/Testing/ProcessingCodeEditor/Compile.js
@@ -1,1 +1,18 @@
+function compile() {
+  var html = ""
+  var pjs = document.getElementById("pjs");
+  var code = document.getElementById("code").contentWindow.document;
 
+  document.body.onkeyup = function() {
+    code.open();
+    code.writeln(
+      html +
+      "<script type=\"text/processing\">" +
+      pjs.value +
+      "</script>"
+    );
+    code.close();
+  };
+}
+
+compile();
