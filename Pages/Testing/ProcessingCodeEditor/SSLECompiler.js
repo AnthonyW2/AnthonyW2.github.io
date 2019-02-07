d3f9b04a3a5cc1685f8fc9a360bc9e2bfe98830e
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-02-07 19:20:22 +1100

    Update SSLECompiler.js

--- Pages/Testing/ProcessingCodeEditor/SSLECompiler.js
+++ Pages/Testing/ProcessingCodeEditor/SSLECompiler.js
@@ -12,7 +12,7 @@ function COMPILE(){
     "<body>"+
     "<script type=\"text/processing\" data-processing-target=\"processing-canvas\">"+
     "void setup(){size(1024,704);frameRate(100);}"+
-    "int LAG = 2;float blockSize = round(1024/30);"+
+    "int LAG = "+document.getElementById("LAG").value+";float blockSize = round(1024/30);"+
     "void wall(float x,float y,float endX,float endY){rect(x,y,endX,endY);};"+
     "void platform(float x,float y,float endX){rect(x,y,endX,2);};"+
     "void ladder(float x, float y, int size){rect(x,y,2,size*blockSize);rect(x+blockSize-2,y,2,size*blockSize);for(int a = 0;a < size*blockSize;a += 14){rect(x,y+a,blockSize,3);}};"+
@@ -33,6 +33,10 @@ function compile(){
   document.body.onkeyup = function() {
     COMPILE();
   };
-}
+};
+function slider(){
+  document.getElementById("lag").innerHTML = document.getElementById("LAG").value;
+};
+
 compile();
 COMPILE();
