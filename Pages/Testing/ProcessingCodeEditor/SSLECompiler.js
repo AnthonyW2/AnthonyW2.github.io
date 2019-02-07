384d723b1642bb61e19a717c6cde7fd6641038d8
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-02-07 12:19:58 +1100

    Update SSLECompiler.js

--- Pages/Testing/ProcessingCodeEditor/SSLECompiler.js
+++ Pages/Testing/ProcessingCodeEditor/SSLECompiler.js
@@ -15,6 +15,7 @@ function COMPILE(){
     "int LAG = 2;float blockSize = round(1024/30);"+
     "void wall(float x,float y,float endX,float endY){rect(x,y,endX,endY);};"+
     "void platform(float x,float y,float endX){rect(x,y,endX,2);};"+
+    "void ladder(float x, float y, int size){rect(x,y,2,size*blockSize);rect(x+blockSize-2,y,2,size*blockSize);for(int a = 0;a < size*blockSize;a += 14){rect(x,y+a,blockSize,3);}};"+
     "void draw(){\n"+
     pjs.value+
     "\n};"+
