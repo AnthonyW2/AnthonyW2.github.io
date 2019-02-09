2f3c11934df4ecae4fe7b910cd37ce3ffbe1746f
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-02-10 08:35:55 +1100

    Update SSLECompiler.js

--- Pages/Testing/ProcessingCodeEditor/SSLECompiler.js
+++ Pages/Testing/ProcessingCodeEditor/SSLECompiler.js
@@ -16,9 +16,10 @@ function COMPILE(){
     "<script type=\"text/processing\" data-processing-target=\"processing-canvas\">"+
     "void setup(){size(1024,704);frameRate(100);}"+
     "int LAG = "+document.getElementById("LAG").value+";float blockSize = round(1024/30);boolean[] triggers = {"+Triggers[0]+","+Triggers[1]+","+Triggers[2]+","+Triggers[3]+","+Triggers[4]+"};"+
-    ""+
-    ""+
-    ""+
+    "float coordsToRadians(float x, float y){if(x >= 0 && y >= 0){return atan(x/y);}else if(x >= 0 && y < 0){return PI+atan(x/y);}else if(x < 0 && y < 0){return PI+atan(x/y);}else if(x < 0 && y >= 0){return TWO_PI+atan(x/y);}else{return 0/0;}}"+
+    "float coordsToAngleRads(float x1, float y1, float x2, float y2){return (coordsToRadians(x1-x2,-y1+y2)+PI)%TWO_PI;}"+
+    "float radiansToCoords(float x1, float y1, float Angle, float distance, float axis){float angle = Angle-HALF_PI;if(Angle < 0){angle = -HALF_PI;}float x = x1 + cos(angle) * distance;float y = y1 + sin(angle) * distance;if(axis == 0){return x;}else{return y;}}"+
+    "float[] enemyPath(float[][] positions, int Step, float Speed){float[] Outputs = {positions[0][0],positions[0][1]};int TP = 1;float xAdd = 0;float yAdd = 0;float retargetCooldown = 0;for(int Frame = 0;Frame < Step;Frame += 1){float AngleInRads = coordsToAngleRads(Outputs[0], Outputs[1], positions[TP][0], positions[TP][1]);xAdd = radiansToCoords(0,0,AngleInRads,Speed,0);yAdd = radiansToCoords(0,0,AngleInRads,Speed,1);Outputs[0] += xAdd;Outputs[1] += yAdd;if(dist(Outputs[0], Outputs[1], positions[TP][0], positions[TP][1]) < 1.5 && retargetCooldown == 0){TP = (TP+1)%positions.length;retargetCooldown = 3;}if(retargetCooldown > 0){retargetCooldown -= 1;}}return Outputs;}"+
     "void wall(float x,float y,float endX,float endY){rect(x,y,endX,endY);};"+
     "void platform(float x,float y,float endX){rect(x,y,endX,2);};"+
     "void ladder(float x, float y, int size){rect(x,y,2,size*blockSize);rect(x+blockSize-2,y,2,size*blockSize);for(int a = 0;a < size*blockSize;a += 14){rect(x,y+a,blockSize,3);}};"+
