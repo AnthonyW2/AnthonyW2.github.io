feacec49e0da295081a651e3e860a9e7733ba816
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-06-04 07:53:39 +1000

    Update MapJS.js

--- Pages/BSCMap/MapJS.js
+++ Pages/BSCMap/MapJS.js
@@ -2,7 +2,6 @@
 var DeviceLongitude = 145.363500;
 var DeviceLatitude = -38.031000;
 function updatePosition(position) {
-  //x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
   DeviceLongitude = position.coords.longitude;
   DeviceLatitude = position.coords.latitude;
 }
@@ -10,7 +9,6 @@ function getLocation(){
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(updatePosition);
   }else{
-    //x.innerHTML = "Geolocation is not supported by this browser.";
     DeviceLongitude = 145.363500;
     DeviceLatitude = -38.031000;
     alert("Geolocation Failure. Please contact Anthony Wilson for help.\nIt is possible that you have location service turned off.");
@@ -24,6 +22,7 @@ var CenterLatitude = -38.031000;
 var SchoolMap = L.map('mapid').setView([CenterLatitude, CenterLongitude], 17);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  minZoom: 16,
   maxZoom: 20,
   id: 'mapbox.streets',
   accessToken: 'pk.eyJ1IjoiYW50aG9ueS13LXByb2dyYW1taW5nIiwiYSI6ImNqd2ZuaGY4YTE3amM0YW91ZHB1ZHZ5NGUifQ.hBTnWAx0ulkkWihsysRrCw'
