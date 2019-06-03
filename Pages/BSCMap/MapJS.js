11c4c3d2a872b28067c727318b83f242ee6586d6
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-06-03 20:50:04 +1000

    Update MapJS.js

--- Pages/BSCMap/MapJS.js
+++ Pages/BSCMap/MapJS.js
@@ -1,3 +1,4 @@
+//This code is made and owned by Anthony Wilson (awilsonprogramming@gmail.com) and is for public use by Berwick Secondary College Interactive School Map
 var DeviceLongitude = 145.363500;
 var DeviceLatitude = -38.031000;
 function updatePosition(position) {
@@ -20,18 +21,18 @@ getLocation();
 var CenterLongitude = 145.363500;
 var CenterLatitude = -38.031000;
 
-var mymap = L.map('mapid').setView([CenterLatitude, CenterLongitude], 17);
+var SchoolMap = L.map('mapid').setView([CenterLatitude, CenterLongitude], 17);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 20,
   id: 'mapbox.streets',
   accessToken: 'pk.eyJ1IjoiYW50aG9ueS13LXByb2dyYW1taW5nIiwiYSI6ImNqd2ZuaGY4YTE3amM0YW91ZHB1ZHZ5NGUifQ.hBTnWAx0ulkkWihsysRrCw'
-}).addTo(mymap);
-var marker = L.marker([DeviceLatitude, DeviceLongitude]).addTo(mymap);
-marker.bindPopup("<b>You are here</b><br>Longitude: "+DeviceLongitude+", Latitude: "+DeviceLatitude).openPopup();
+}).addTo(SchoolMap);
+var DeviceMarker = L.marker([DeviceLatitude, DeviceLongitude]).addTo(SchoolMap);
+DeviceMarker.bindPopup("<b>You are here</b><br>Longitude: "+DeviceLongitude+", Latitude: "+DeviceLatitude).openPopup();
 
 function panMap(Lat, Lng){
-  mymap.panTo(new L.LatLng(Lat, Lng));
+  SchoolMap.panTo(new L.LatLng(Lat, Lng));
 }
 
 var frameCount = 0;
@@ -48,7 +49,7 @@ function draw(){
   
   getLocation();
   var newLatLng = new L.LatLng(DeviceLatitude, DeviceLongitude);
-  marker.setLatLng(newLatLng);
+  DeviceMarker.setLatLng(newLatLng);
   
   document.getElementById("LatLng").innerHTML = "Latitude: "+DeviceLatitude+", Longitude: "+DeviceLongitude;
   
