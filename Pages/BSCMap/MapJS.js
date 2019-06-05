b52f2ba20b0cc2081f1cfcb836351a87c11455a1
Anthony Wilson <41666161+Anthony-Wilson-Programming@users.noreply.github.com>
2019-06-05 13:22:51 +1000

    Update MapJS.js

--- Pages/BSCMap/MapJS.js
+++ Pages/BSCMap/MapJS.js
@@ -38,6 +38,7 @@ var imageUrl = 'https://anthony-wilson-programming.github.io/Pages/BSCMap/BasicL
 var imageBounds = [[145.363500, -38.031000], [145.353500, -38.021000]];
 var DeviceMarker = L.marker([DeviceLatitude, DeviceLongitude]).addTo(SchoolMap);
 DeviceMarker.bindPopup("<b>You are here</b>").openPopup();
+L.imageOverlay(imageUrl, imageBounds).addTo(SchoolMap);
 
 function panMap(Lat, Lng){
   SchoolMap.panTo(new L.LatLng(Lat, Lng));
@@ -69,7 +70,5 @@ function draw(){
     setTimeout(draw,Math.ceil(MSPT));
   }
 }
-
-L.imageOverlay(imageUrl, imageBounds).addTo(map);alert("Image Success");
 
 draw();
